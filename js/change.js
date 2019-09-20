c332cbec0331c49e73320d8868fd2b51ee213660
GlennOnyango <glennonyango@yahoo.com>
2019-09-20 18:42:19 +0300

    Donate is upgraded

--- js/change.js
+++ js/change.js
@@ -1,35 +1,76 @@
-$(document).ready(function(){
-   
-    $('#home').click(function(){
-        $('#fdp_a').addClass("hiddendiv");
-        $('#acd_a').addClass('hiddendiv');
-        $('#home_page').removeClass("hiddendiv");
-        $('#cara').removeClass("hide");
-    });
+$(document).ready(function () {
 
-    $('#academics').click(function(){
-        $('#acd_a').removeClass('hiddendiv');
-        $('#fdp_a').addClass("hiddendiv");
-        $('#home_page').addClass("hiddendiv");
-        $('#cara').addClass("hide");
+
+    cards = ['#cr', '#fp', '#st', '#lm', '#pg', '#tl', '#tc', '#cp', '#lb'];
+
+
+setTimeout(() => {
+    $(cards[0]).removeClass('hiddendiv');
+ 
+    setTimeout(() => {
+        $(cards[1]).removeClass('hiddendiv');
+     
+setTimeout(() => {
+    $(cards[2]).removeClass('hiddendiv');
+ 
+setTimeout(() => {
+    $(cards[3]).removeClass('hiddendiv');
+ 
+setTimeout(() => {
+    $(cards[4]).removeClass('hiddendiv');
+ 
+    setTimeout(() => {
+        $(cards[5]).removeClass('hiddendiv');
+  
+setTimeout(() => {
+    $(cards[6]).removeClass('hiddendiv');
+ 
+    setTimeout(() => {
+        $(cards[7]).removeClass('hiddendiv');
+ 
+setTimeout(() => {
+    $(cards[8]).removeClass('hiddendiv');
+ 
+}, 1000);
+
+    }, 1000);
+
+}, 1000);
+
+    }, 1000);
+
+}, 1000);
+
+}, 1000);
+
+}, 1000);
+
+    }, 1000);
+
+}, 1000);
+
+
+
+    /*for (let index = 0; index < 9; index++) {
+
+        interval = 3000;
+
+        setTimeout(() => {
+            $(cards[2]).removeClass('hiddendiv');
+        }, interval);
+
+interval -= 100;
+
+    }*/
+
+    /*cards.forEach(element => {
         
-    });
+        
+        setTimeout(() => {
+            $(element).removeClass('hiddendiv');
+        }, 2000);
 
-
-    $('#FDP').click(function(){
-        $('#fdp_a').removeClass("hiddendiv");
-        $('#home_page').addClass("hiddendiv");
-        $('#cara').addClass("hide");
-        $('#acd_a').addClass('hiddendiv');
     });
-    
-   
-    $('#donation-btn').click(function(){
-        $('#donations-page').removeClass("hiddendiv");
-        $('#fdp_a').addClass("hiddendiv");
-        $('#home_page').addClass("hiddendiv");
-        $('#cara').addClass("hide");
-        $('#acd_a').addClass('hiddendiv'); 
-       });
+*/
 
 });
